934fbfa82f779a2d94111f26c4cce7301b88d509
Ghada Khalil <ghada.khalil@windriver.com>
2021-05-11 01:24:00 +0000

    Revert "Migrate nginx overrides from old to new chart"
    This reverts commit 9189e5bc0ac06d44f0bda8ab70ba151b05349dca.
    
    Reason for revert: Causes an ansible failure on fresh system install
    
    Change-Id: I4f1263c2cf0957d3bb5e324b9ac7fdd8d5beda69

--- python-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/common/constants.py
+++ python-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/common/constants.py
@@ -8,4 +8,3 @@ from sysinv.helm import common
 
 HELM_NS_NGINX_INGRESS_CONTROLLER = common.HELM_NS_KUBE_SYSTEM
 HELM_CHART_INGRESS_NGINX = 'ingress-nginx'
-HELM_CHART_LEGACY_INGRESS_NGINX = 'nginx-ingress'
