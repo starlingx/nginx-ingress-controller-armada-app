1f419fd6534236ba37bb7393e7f7653f2159bd20
Lucas Cavalcante <lucasmedeiros.cavalcante@windriver.com>
2022-06-30 13:46:26 -0300

    Fix Nginx Controller Helm plugin name collision
    If a Helm plugin from app (X) has the same name as other plugin from a
    different app (Y), app plugins from X is not installed as there is a
    plugin with the same name already installed. Internally Sysinv
    AppOperator has a HashTable and its key is the plugin name, therefore
    preventing adding plugins with same name.
    
    We change nginx plugin name so that other apps with different nginx
    pĺugins don't collide and prevent the platform from being able to
    apply/reapply nginx-platfrom-app
    
    001_ingres-nginx -> 0001_ks-ingress-nginx
    
    Test Plan:
    PASS: Apply Nginx together with an app with previous nginx plugin
    name
    PASS: Name changed at system overrides, command:
    system helm-override-show nginx-ingress-controller
    nginx-ingress-controller kube-system
    
    Closes-bug: 1980394
    Signed-off-by: Lucas Cavalcante <lucasmedeiros.cavalcante@windriver.com>
    Change-Id: I2a1b663d3867873d024c108d71ee074da1a39a5e

--- python-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/common/constants.py
+++ python-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/common/constants.py
@@ -7,6 +7,6 @@
 from sysinv.helm import common
 
 HELM_NS_NGINX_INGRESS_CONTROLLER = common.HELM_NS_KUBE_SYSTEM
-HELM_CHART_INGRESS_NGINX = 'ingress-nginx'
+HELM_CHART_INGRESS_NGINX = 'ks-ingress-nginx'
 HELM_CHART_LEGACY_INGRESS_NGINX = 'nginx-ingress'
 CHART_GROUP_INGRESS_NGINX = 'nginx-ingress'
