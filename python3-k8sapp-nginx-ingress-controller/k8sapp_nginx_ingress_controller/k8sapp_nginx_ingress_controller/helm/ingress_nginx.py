afd4594d85a2249b27c5b1132a896fd580d6b379
Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>
2026-04-21 08:18:30 -0400

    Add TLS configuration to nginx-ingress helm overrides
    Add platform TLS service parameter support to the nginx-ingress
    helm overrides so the ConfigMap gets correct ssl-protocols and
    ssl-ciphers values on initial app apply and reapply.
    
    - Add _get_tls_config() to read platform TLS service params
      from DB and convert to nginx format
    - Add ssl-protocols and ssl-ciphers to controller.config in
      get_overrides()
    - IANA to OpenSSL cipher conversion for TLS 1.2 ciphers
    - TLS 1.3 ciphers pass through as IANA names (nginx accepts
      both formats)
    - DB failure falls back to TLS 1.2 defaults
    
    Test Plan:
    - Integration tests (AIO-SX):
      - Verify nginx-ingress ConfigMap has ssl-protocols and
        ssl-ciphers after service-parameter-apply platform
      - Verify port 443 TLS negotiation works with configured
        ciphers
      - Modify tls-min-version to VersionTLS13 and apply
        Verify port 443 rejects TLS 1.2 connections
      - Restore to VersionTLS12 and apply
        Verify port 443 accepts TLS 1.2 again
      - No alarms after full apply/restore cycle
    
    Depends-On: https://review.opendev.org/c/starlingx/config/+/985645
    
    Story: 2011725
    Task: 54192
    
    Change-Id: I134fd9a12a25977b3d70db4f0660404616af60cd
    Signed-off-by: Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>

--- python3-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/helm/ingress_nginx.py
+++ python3-k8sapp-nginx-ingress-controller/k8sapp_nginx_ingress_controller/k8sapp_nginx_ingress_controller/helm/ingress_nginx.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2022 Wind River Systems, Inc.
+# Copyright (c) 2022-2026 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,6 +10,21 @@ from sysinv.common import exception
 from sysinv.helm import base
 
 LOG = log.getLogger(__name__)
+
+IANA_TO_OPENSSL_CIPHER_MAP = {
+    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384':
+        'ECDHE-RSA-AES256-GCM-SHA384',
+    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256':
+        'ECDHE-RSA-AES128-GCM-SHA256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384':
+        'ECDHE-ECDSA-AES256-GCM-SHA384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256':
+        'ECDHE-ECDSA-AES128-GCM-SHA256',
+    'TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256':
+        'ECDHE-RSA-CHACHA20-POLY1305',
+    'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256':
+        'ECDHE-ECDSA-CHACHA20-POLY1305',
+}
 
 
 class IngressNginxHelm(base.BaseHelm):
@@ -26,12 +41,57 @@ class IngressNginxHelm(base.BaseHelm):
             [app_constants.HELM_NS_NGINX_INGRESS_CONTROLLER]
     }
 
+    def _get_tls_config(self):
+        """Get TLS ssl-protocols and ssl-ciphers from service parameters."""
+        tls_min_version = \
+            constants.SERVICE_PARAM_PLATFORM_TLS_MIN_VERSION_DEFAULT
+        tls_cipher_suite = \
+            constants.SERVICE_PARAM_PLATFORM_TLS_CIPHER_SUITE_DEFAULT
+
+        try:
+            parms = self.dbapi.service_parameter_get_all(
+                service=constants.SERVICE_TYPE_PLATFORM,
+                section=constants.SERVICE_PARAM_SECTION_PLATFORM_CONFIG)
+            for p in parms:
+                if p.name == \
+                        constants.SERVICE_PARAM_NAME_PLATFORM_TLS_MIN_VERSION:
+                    tls_min_version = p.value
+                elif p.name == \
+                        constants.SERVICE_PARAM_NAME_PLATFORM_TLS_CIPHER_SUITE:
+                    tls_cipher_suite = p.value
+        except Exception:
+            LOG.warning("Failed to read TLS service parameters, "
+                        "using defaults for nginx-ingress overrides")
+
+        if tls_min_version == \
+                constants.SERVICE_PARAM_PLATFORM_TLS_VERSION_TLS13:
+            ssl_protocols = 'TLSv1.3'
+        else:
+            ssl_protocols = 'TLSv1.2 TLSv1.3'
+
+        openssl_ciphers = []
+        for iana_name in tls_cipher_suite.split(','):
+            iana_name = iana_name.strip()
+            if not iana_name:
+                continue
+            openssl_name = IANA_TO_OPENSSL_CIPHER_MAP.get(
+                iana_name, iana_name)
+            openssl_ciphers.append(openssl_name)
+
+        return ssl_protocols, ':'.join(openssl_ciphers)
+
     def get_overrides(self, namespace=None):
         LOG.info("Generating system_overrides for %s chart." % self.CHART)
+
+        ssl_protocols, ssl_ciphers = self._get_tls_config()
 
         overrides = {
             app_constants.HELM_NS_NGINX_INGRESS_CONTROLLER: {
                 'controller': {
+                    'config': {
+                        'ssl-protocols': ssl_protocols,
+                        'ssl-ciphers': ssl_ciphers,
+                    },
                     'service': {
                         'ipFamilyPolicy': 'PreferDualStack',
                         'ipFamilies': []
